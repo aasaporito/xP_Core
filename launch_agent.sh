864b8df1a82290a4fdaf8519ec7f58e2928b926d
Aaron <sapblatt11@gmail.com>
2024-02-15 13:32:43 -0500

    Added nohup

--- launch_agent.sh
+++ launch_agent.sh
@@ -1,3 +1,3 @@
 echo "Launching 2 instances";
-python3 ~/Documents/xP_Core/core_controller.py $RANDOM &
-python3 ~/Documents/xP_Core/core_controller.py $RANDOM &
+nohup python3 ~/Documents/xP_Core/core_controller.py $RANDOM &
+#python3 ~/Documents/xP_Core/core_controller.py $RANDOM &
