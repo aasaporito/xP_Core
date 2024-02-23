2c9c080bee867f6334e9db472bd4746e82500daf
Aaron <sapblatt11@gmail.com>
2024-02-23 15:25:24 -0500

    Removed seconcary bash script calls

--- run_core.sh
+++ run_core.sh
@@ -14,13 +14,14 @@ echo "Purged traceback folder"
 # Start Server
 echo "Starting Xpilots Server";
 # switchBase 1 = 100% probability to swap bases on death, + teams disables teams
-ssh -X -J asaporito@grid asaporito@lab01 "bash -s" < ~/Documents/xP_Core/start_server.sh &
+ssh -X asaporito@lab01 ~/Documents/xP_Core/xpilots -map ~/Documents/xP_Core/simple.xp -noquit -switchBase 1.0 +teams -maxRoundTime 60 -roundsToPlay 0 -resetOnHuman 1 -limitedLives -maxClientsPerIP 32 &
 sleep 2;
 
 # Set the number of instances you want to run
 machines=(
     lab02
-    
+    lab03
+
     )
 
 #echo "Running instances"
@@ -31,7 +32,7 @@ machines=(
 for machine in "${machines[@]}" 
 do
     echo "Connecting to: "$machine;
-    ssh -X -J asaporito@grid asaporito@$machine "nohup bash -s" < ~/Documents/xP_Core/launch_agent.sh &
+    ssh -X asaporito@$machine "~/Documents/xP_Core/core_controller.py $RANDOM" &
 done
 
 # Labs1-20, slrm 1,3 4-14, 17
