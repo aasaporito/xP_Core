9c5046c2dcae06783441c26e6513dfff25eac0b9
Aaron <sapblatt11@gmail.com>
2024-04-05 22:05:31 -0400

    Added error logging to QS

--- QueueServer/queue_server.py
+++ QueueServer/queue_server.py
@@ -25,6 +25,22 @@ queues = [queue_1, queue_2, queue_3, queue_4]
 app = FastAPI() 
 
 
+import logging
+from fastapi import FastAPI, Request, status
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+
+app = FastAPI()
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    exc_str = f'{exc}'.replace('\n', ' ').replace('   ', ' ')
+    logging.error(f"{request}: {exc_str}")
+    content = {'status_code': 10422, 'message': exc_str, 'data': None}
+    return JSONResponse(content=content, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+
 @app.post("/update_map")
 async def update_map(data: ChromMap):
     #print(data)
