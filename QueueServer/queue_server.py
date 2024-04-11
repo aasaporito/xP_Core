da01460873c8b4a89cc98dad09ef9ad16168f9c4
Aaron <sapblatt11@gmail.com>
2024-04-11 17:34:16 -0400

    Fixed config issue for QS

--- QueueServer/queue_server.py
+++ QueueServer/queue_server.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-from .. import config
+import sys
+import os
+current_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
 
+import config
 
 class ChromeID(BaseModel):
     quadrant: int = -1
