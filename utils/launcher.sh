310bcf1666a23047db692fb44bf6485523d9b220
Aaron <sapblatt11@gmail.com>
2024-04-12 00:25:13 -0400

    Test of launcher

--- utils/launcher.sh
+++ utils/launcher.sh
@@ -1,4 +1,4 @@
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM > /dev/null 2>&1 &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
 python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM > /dev/null 2>&1 &
 python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM > /dev/null 2>&1 &
 python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM > /dev/null 2>&1 &
