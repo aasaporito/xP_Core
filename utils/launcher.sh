8498959c54089d14f103a4fe2560bea3f485c914
Aaron <sapblatt11@gmail.com>
2024-04-12 00:26:36 -0400

    Test of launcher

--- utils/launcher.sh
+++ utils/launcher.sh
@@ -1,35 +1,35 @@
 python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM > /dev/null 2>&1 &
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM > /dev/null 2>&1 &
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM > /dev/null 2>&1 &
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM > /dev/null 2>&1 &
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM > /dev/null 2>&1 &
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM > /dev/null 2>&1 &
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM > /dev/null 2>&1 &
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM > /dev/null 2>&1 &
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM > /dev/null 2>&1 &
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM > /dev/null 2>&1 &
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM > /dev/null 2>&1 &
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM > /dev/null 2>&1 &
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM > /dev/null 2>&1 &
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM > /dev/null 2>&1 &
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM > /dev/null 2>&1 &
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM > /dev/null 2>&1 &
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM > /dev/null 2>&1 &
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM > /dev/null 2>&1 &
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM > /dev/null 2>&1 &
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM > /dev/null 2>&1 &
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM > /dev/null 2>&1 &
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM > /dev/null 2>&1 &
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM > /dev/null 2>&1 &
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM > /dev/null 2>&1 &
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM > /dev/null 2>&1 &
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM > /dev/null 2>&1 &
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM > /dev/null 2>&1 &
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM > /dev/null 2>&1 &
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM > /dev/null 2>&1 &
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM > /dev/null 2>&1 &
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM > /dev/null 2>&1 &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
 
 echo "Launched 32 agents on machine, exiting.";
 exit;
