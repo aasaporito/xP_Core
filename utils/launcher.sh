949a3f3b50eb5a5855cde3f8d40daf138b7836c0
Aaron <sapblatt11@gmail.com>
2024-04-15 16:38:59 -0400

    Working on baxkgrounding run

--- utils/launcher.sh
+++ utils/launcher.sh
@@ -1,35 +1,36 @@
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM
-python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+python3 ~/Documents/xP_Core/src/core_controller.py $RANDOM &
+
 
 echo "Launched 32 agents on machine, exiting.";
 exit;
