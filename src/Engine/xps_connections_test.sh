794bb094b05fd6bdd4f449700c7c24defbd856d8
Aaron <sapblatt11@gmail.com>
2024-04-15 12:27:33 -0400

    Added client connection tester script

---
+++ src/Engine/xps_connections_test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Loop from 1 to 32
+for ((i = 1; i <= 32; i++)); do
+  (
+    # Call the command, replacing "5" with the current iteration number
+    ./xpilot -join localhost -name X"$i" &
+    sleep 0.5;
+    )
+done
